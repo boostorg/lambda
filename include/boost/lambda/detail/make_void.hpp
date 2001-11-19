7dd0e88f2bfa04ecbfc180486c023481551187ee
Jaakko Järvi <jarvi@cs.tamu.edu>
2001-11-19 22:11:10 +0000

    RET static -> static RET
    [SVN r11735]

--- include/boost/lambda/detail/make_void.hpp
+++ include/boost/lambda/detail/make_void.hpp
@@ -28,7 +28,7 @@ namespace lambda {
 
 template<> struct return_void_action<other_action<identity_action> > {
   template<class RET, class A>
-  RET static apply(A& a) { a; }
+  static RET apply(A& a) { a; }
 };
 
 
